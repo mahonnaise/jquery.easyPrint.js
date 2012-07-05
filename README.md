c4e113508d0a3d5230e86d96e2fc440de2e52044
Jos Hirth <github@kaioa.com>
2012-07-05 03:06:10 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+#jquery.easyPrint.js ([Demo](http://kaioa.com/k/test/easyPrint/demo/index.html))
+
+*jquery.easyPrint.js* is a jQuery plugin which allows you to print the contents of one or several elements (a jQuery collection).
+
+Stylesheets are automatically used if they were included like this:
+
+```html
+<link rel="stylesheet" [...]/>
+```
+
+#Usage
+
+<code>$(<var>selector</var>).easyPrint(&lt;<var>title</var>&gt;);</code>
+
+* `selector` &ndash; a jQuery [selector](http://api.jquery.com/category/selectors/)`
+* `title` &ndash; the document title (optional)
+
+#Compatibility
+
+* any recent browser (Opera uses a popup instead of an `iframe`)
+* IE versions as old as 6
---
+++ jquery.easyPrint.js
@@ -0,0 +1,73 @@
+/*! github.com/mahonnaise/jquery.easyPrint.js */
+
+/*jslint plusplus:false, browser:true, plusplus:true*/
+/*global jQuery*/
+
+(function ($) {
+	'use strict';
+	$.fn.easyPrint = function (title) {
+		var iframe, contentWindow, doc, that = this, html;
+
+		html = '<!DOCTYPE html><html><head>';
+
+		if (title !== undefined) {
+			html += '<title>' + title + '</title>';
+		}
+
+		// Simply using:
+		//   $(that[0].ownerDocument).find('link[rel="stylesheet"]').html()
+		// doesn't work. It returns an empty string with Firefox 4-13.
+		html += (function () {
+			var markup = '', i, len, stylesheets;
+			stylesheets = $(that[0].ownerDocument).find('link[rel="stylesheet"]');
+			len = stylesheets.length;
+			for (i = 0; i < len; i++) {
+				markup += '<link rel="stylesheet" href="' + stylesheets[i].href + '"' +
+					(stylesheets[i].media ? (' media="' + stylesheets[i].media + '"') : '') +
+					'/>';
+			}
+			return markup;
+		}());
+
+		html += '</head><body>';
+		
+		this.each(function () {
+			html += $(this).html();
+		});
+		
+		html += '</body></html>';
+		
+		if ($.browser.opera) {
+			contentWindow = window.open(
+				'data:,',
+				title,
+				'menubar=false,width=640,height=480'
+			);
+		} else {
+			iframe = $('<iframe tabindex=-1/>');
+			// The iframe must be visible for IE. visibility:hidden can't be used.
+			iframe.css({
+				position: 'absolute',
+				height: 1,
+				top: -10
+			});
+			$(document.body).append(iframe);
+
+			contentWindow = iframe[0].contentWindow;
+		}
+		doc = contentWindow.document;
+		
+		doc.open();
+		doc.write(html);
+		doc.close();
+
+		// Opera requires some rescheduling. It appears that Opera changes the focus (in response to clicks) after notifying the listeners.
+		// This means we have to postpone focus() by one step. Otherwise it would get undone by Opera's native focus handling.
+		window.setTimeout(function () {
+			contentWindow.focus();
+			contentWindow.print();
+		}, 0);
+
+		return this;
+	};
+}(jQuery));
